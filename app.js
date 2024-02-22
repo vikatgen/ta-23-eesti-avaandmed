b83907dfd61cb1dbc887d8f4079720174e4f9147
Gen Vikat <gen@veebisepad.ee>
2024-02-22 10:05:11 +0200

    add options to select

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const selectHTMLElement = document.querySelector("#open-data");
+
 const baseURL = "https://avaandmed.eesti.ee/api/datasets/";
 
 fetch(baseURL)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then(({ data, metadata }) => {
+        console.log(metadata);
+        data.forEach((category) => {
+            const optionHTMLElement = new Option(category.name, category.id);
+            selectHTMLElement.appendChild(optionHTMLElement);
+            console.log(category);
+        });
+    });
